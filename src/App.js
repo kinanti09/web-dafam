31b21d7e9b221e6a7254eb82b7f64772645cf80c
kinanti09 <kinantianjang09@gmail.com>
2019-10-03 23:47:14 +0700

    drawer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Drawer from './components/drawer/component';
+
+function App() {
+  return (
+    <div className="App">
+      <Drawer />
+    </div>
+  );
+}
+
+export default App;
